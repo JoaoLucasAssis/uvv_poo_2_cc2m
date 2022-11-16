a125fddc03ef06bc862519471e782eebda981e54
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2022-11-16 20:12:31 -0300

    commit

---
+++ Jogo modelado/JogoModelado/src/Arma_IF.java
@@ -0,0 +1,5 @@
+
+public class Arma_IF implements Revolver, Fuzil, Desarmado {
+    
+    public void usarArma(){}
+}
---
+++ Jogo modelado/JogoModelado/src/Personagem.java
@@ -0,0 +1,15 @@
+
+public class Personagem {
+    
+    protected Arma_IF arma;
+    
+    public void desenhar(){}
+    
+    public void falar(){}
+    
+    public void arma(){
+        arma.usarArma();
+    }
+    
+    public void SetArma(Arma_IF arma){}
+}
